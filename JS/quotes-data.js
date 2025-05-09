9a9dcf84c1eec0b09a3e81c685ec01f0fdc10555
kcsanju444 <kcsanju600@gmaill.com>
2025-05-09 11:49:45 +0545

    Add categorized quotes in quotes-data.js

---
+++ JS/quotes-data.js
@@ -0,0 +1,19 @@
+const quote=[{
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+    category: "motivational",
+},
+{
+    quote: "Don’t watch the clock; do what it does. Keep going.",
+    author: "Sam Levenson",
+    category: "motivational",
+},{
+    quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: " Winston S. Churchill",
+    category: "motivational",
+},{
+    quote: "Hardships often prepare ordinary people for an extraordinary destiny.",
+    author: "  C.S. Lewis",
+    category: "motivational",
+},
+]
