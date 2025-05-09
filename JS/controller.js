6d1b61273fdec9bc712480404062c7392e725dd6
kcsanju444 <kcsanju600@gmaill.com>
2025-05-09 13:15:57 +0545

    create the function for copy and font size

--- JS/controller.js
+++ JS/controller.js
@@ -13,30 +13,58 @@ function displayQuote() {
   quoteText.innerText = current.quote;
   authorText.innerText = `-- ${current.author}`;
 }
+
 function generateRandomQuote() {
-    let randomIndex;
-    do {
-      randomIndex = Math.floor(Math.random() * selectedQuoteList.length);
-    } while (randomIndex === quoteIndex);
-    quoteIndex = randomIndex;
-    displayQuote();
-  }
-  
-  function nextQuote() {
-    quoteIndex = (quoteIndex + 1) % selectedQuoteList.length;
-    displayQuote();
-  }
-  
-  function previousQuote() {
-    quoteIndex = (quoteIndex - 1 + selectedQuoteList.length) % selectedQuoteList.length;
-    displayQuote();
-  }
-  
-  window.previousQuote = previousQuote;
-  window.nextQuote = nextQuote;
-  window.generateRandomQuote = generateRandomQuote;
-  window.categoryChange = categoryChange;
+  let randomIndex;
+  do {
+    randomIndex = Math.floor(Math.random() * selectedQuoteList.length);
+  } while (randomIndex === quoteIndex);
+  quoteIndex = randomIndex;
   displayQuote();
-  
+}
 
+function nextQuote() {
+  quoteIndex = (quoteIndex + 1) % selectedQuoteList.length;
+  displayQuote();
+}
 
+function previousQuote() {
+  quoteIndex = (quoteIndex - 1 + selectedQuoteList.length) % selectedQuoteList.length;
+  displayQuote();
+}
+
+function categoryChange(select) {
+  selectedCategory = select.value;
+  selectedQuoteList = quotes.filter(q => q.category === selectedCategory);
+  quoteIndex = 0;
+  displayQuote();
+}
+
+function fontChange(action) {
+  const currentSize = parseFloat(window.getComputedStyle(quoteText).fontSize);
+  if (action === 'add') {
+    quoteText.style.fontSize = `${currentSize + 2}px`;
+  } else if (action === 'sub') {
+    quoteText.style.fontSize = `${currentSize - 2}px`;
+  }
+}
+
+function copyQuote() {
+  const fullQuote = `${quoteText.innerText} ${authorText.innerText}`;
+  navigator.clipboard.writeText(fullQuote).then(() => {
+    alert("Quote copied!");
+  });
+}
+
+toggleSwitch.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode");
+});
+
+window.previousQuote = previousQuote;
+window.nextQuote = nextQuote;
+window.generateRandomQuote = generateRandomQuote;
+window.categoryChange = categoryChange;
+window.copyQuote = copyQuote;
+window.fontChange = fontChange;
+
+displayQuote();
